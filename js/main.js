61f16a3ab9d3cc090b2ff03a5395f6276695205b
Daniel Enciso <encisoda@gmail.com>
2020-07-20 21:45:36 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 function initCanvas(){
-    const ctx = document.getElementById('canvas').getContext('2d');
-    const fondoCanvas = new Image();
-    const naveImagen = new Image();
-    const enemigoImagen1 = new Image();
-    const enemigoImagen2 = new Image();
+    var ctx = document.getElementById('canvas').getContext('2d');
+    var fondoCanvas = new Image();
+    var naveImagen = new Image();
+    var enemigoImagen1 = new Image();
+    var enemigoImagen2 = new Image();
 
     fondoCanvas.src = "../img/fondoCanvas.jpg";
     naveImagen.src = "../img/nave.png";
     enemigoImagen1.src = "../img/enemigo1.png";
     enemigoImagen2.src = "../img/enemigo2.png";
 
-    const cW = ctx.canvas.width;
-    const cH = ctx.canvas.heigth;
+    var cW = ctx.canvas.width;
+    var cH = ctx.canvas.heigth;
 
-    const enemigoTemplate = function (opciones){
+    var enemigoTemplate = function (opciones){
         return{
             id: opciones.id || '',
             x: opciones.x || '',
@@ -24,23 +24,24 @@ function initCanvas(){
         }
     }
 
-    const enemigos = [
+    var enemigos = [
         new enemigoTemplate({id: "enemigo1", x : 350, y: 50, w: 80, h: 30})
     ];
 
     var mostrarEnemigos = function(arrayEnemigos){
-        for(let i = 0; i < arrayEnemigos.length; i++){
-            let enemigo = arrayEnemigos[i];
+        for(var i = 0; i < arrayEnemigos.length; i++){
+            var enemigo = arrayEnemigos[i];
             ctx.drawImage(enemigo.image, enemigo.x, enemigo.y, enemigo.w, enemigo.h)
         }
     }
-    
+
+
     function animar(){
         ctx.clearRect(0, 0, cW, cH);
         mostrarEnemigos(enemigos);
     }
 
-    const animacionInterna = setInterval(animar, 6);
+    var animacionInterna = setInterval(animar, 6);
 
 }
 
